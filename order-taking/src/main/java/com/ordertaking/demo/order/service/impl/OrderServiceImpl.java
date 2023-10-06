d08be2c2ae4105c3b91b177820f83603a7a487f5
Artur <buzov.artur@gmail.com>
2023-10-07 01:53:41 +0200

    add new topic 'email_new_order'

--- order-taking/src/main/java/com/ordertaking/demo/order/service/impl/OrderServiceImpl.java
+++ order-taking/src/main/java/com/ordertaking/demo/order/service/impl/OrderServiceImpl.java
@@ -12,13 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    private final static String TOPIC_NAME = "order";
+    private final static String TOPIC_NEW_ORDER_NAME = "order";
+    private final static String TOPIC_EMAIL_NEW_ORDER_NAME = "email_new_order";
     private KafkaTemplate<String, Order> kafkaTemplate;
 
     @Override
     public void takeOrder(Order order) {
         // TODO need to save into DB
-        kafkaTemplate.send(TOPIC_NAME, order);
+        kafkaTemplate.send(TOPIC_NEW_ORDER_NAME, order);
+        kafkaTemplate.send(TOPIC_EMAIL_NEW_ORDER_NAME, order);
     }
 
 }
